6a727a20a172c78259b8595571c90cd4b8cef1f5
Kartavya Rupal <kartavyarupal.kr@gmail.com>
2024-11-26 02:25:51 +0530

    Initial Commit - Vite React ToDo App

---
+++ src/contexts/index.js
@@ -0,0 +1,5 @@
+import { TodoContext } from "./TodoContext"
+import { Todoprovider } from "./TodoContext"
+import { useTodo } from "./TodoContext"
+
+export {TodoContext, Todoprovider, useTodo } from "./TodoContext"
